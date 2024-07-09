1ac18bff192a3c35594842663c2b28ce941ab598
wanqian6311 <wanqian6311@gamil.com>
2024-07-09 23:09:46 +0800

    更新

--- shuati/js/public.js
+++ shuati/js/public.js
@@ -1,5 +1,5 @@
 const JSONList = [
-    { id: "questions", name: '商用密码应用安全性评估从业人员考核参考题库', describe: '商用密码应用安全性评估从业人员考核参考题库', file: "questions.json" }
+    { id: "questions", name: '商用密码应用安全性评估从业人员考核参考题库', describe: '商用密码应用安全性评估从业人员考核参考题库——wanqian6311整理', file: "questions.json" }
 
    
 ]
