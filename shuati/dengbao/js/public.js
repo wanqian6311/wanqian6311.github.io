05a6b45b6e6f14f42acf1ede5f1ba653b5e62a57
Levin <135097639+wanqian6311@users.noreply.github.com>
2024-08-08 00:29:59 +0800

    Update public.js

--- shuati/dengbao/js/public.js
+++ shuati/dengbao/js/public.js
@@ -1,4 +1,4 @@
 const JSONList = [
-    { id: "danxaun", name: '【单选题)】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "danxuan.json" }
+    { id: "danxaun", name: '【单选题】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "danxuan.json" }
     
 ]
