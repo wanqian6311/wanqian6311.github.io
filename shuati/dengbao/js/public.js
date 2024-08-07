767eb056bc8e2dcfff91fe9144ae970c6f19fa39
wanqian6311 <wanqian6311@gamil.com>
2024-08-08 01:12:55 +0800

    更新

--- shuati/dengbao/js/public.js
+++ shuati/dengbao/js/public.js
@@ -1,4 +1,6 @@
 const JSONList = [
-    { id: "danxuan", name: '【单选题】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "danxuan.json" }
+    { id: "danxaun", name: '【单选题】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "danxuan.json" },
+    { id: "duoxuan", name: '【多选题】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "duoxuan.json" }
+    
     
 ]
