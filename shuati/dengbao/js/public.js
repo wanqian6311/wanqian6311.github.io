4edade1ae0a2b1ca76ddc979401a257fb2a7b947
Levin <135097639+wanqian6311@users.noreply.github.com>
2024-08-12 16:00:27 +0800

    Update public.js

--- shuati/dengbao/js/public.js
+++ shuati/dengbao/js/public.js
@@ -2,6 +2,7 @@ const JSONList = [
     { id: "danxaun", name: '【单选题】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "danxuan.json" },
     { id: "duoxuan", name: '【多选题】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "duoxuan.json" },
     { id: "panduan", name: '【判断题】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "panduan.json" },
+    { id: "zonghe", name: '【单、多、判断】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "zonghe.json" },
     
     
 ]
