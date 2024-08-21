ae88ece4703a76c1a11dbc82d42859cf61de78d1
Levin <135097639+wanqian6311@users.noreply.github.com>
2024-08-21 15:58:28 +0800

    Update public.js

--- shuati/dengbao/js/public.js
+++ shuati/dengbao/js/public.js
@@ -3,7 +3,7 @@ const JSONList = [
     { id: "duoxuan", name: '【多选题】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "duoxuan.json" },
     { id: "panduan", name: '【判断题】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "panduan.json" },
     { id: "zonghe", name: '【单、多、判断】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "zonghe.json" },
-    { id: "dengbao", name: '等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "https://files.cnblogs.com/files/blogs/827792/dengbao.json?t=1724226646&download=true" },
+
     
     
 ]
