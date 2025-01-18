10e91b27ea8d06c7650c375ee2fbcdf80529b9b4
wanqian6311 <wanqian6311@163.com>
2025-01-19 01:07:29 +0800

    自动提交

---
+++ shuati/dengbao/js/public.js
@@ -0,0 +1,9 @@
+const JSONList = [
+    { id: "danxaun", name: '【单选题】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "danxuan.json" },
+    { id: "duoxuan", name: '【多选题】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "duoxuan.json" },
+    { id: "panduan", name: '【判断题】等保参考题库', describe: '等保参考题库——wanqian6311整理', file: "panduan.json" },
+    
+
+    
+    
+]
