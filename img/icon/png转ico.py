a2f34f8dbe6f4abd41bdac30b41baad19667e7b0
wanqian6311 <135097639+wanqian6311@users.noreply.github.com>
2025-04-24 10:16:53 +0800

    Update png转ico.py

--- img/icon/png转ico.py
+++ img/icon/png转ico.py
@@ -1,8 +1,11 @@
 from PIL import Image
 import os
 
+png_file = "touxiang.png"          # 你的 PNG 文件
+output_folder = "ico_outputs"     # 输出目录
+
 def generate_individual_ico(png_path, output_dir):
-    sizes = [16, 32, 48, 72, 96, 128, 144, 180, 192, 256, 512]
+    sizes = [16, 32, 48, 72, 96, 128, 144, 180, 192, 256, 500, 512]
 
     # 创建输出目录（如果不存在）
     os.makedirs(output_dir, exist_ok=True)
@@ -21,8 +24,13 @@ def generate_individual_ico(png_path, output_dir):
         resized.save(ico_filename, format='ICO')
         print(f"生成: {ico_filename}")
 
+    # 生成logo.png 大小为500x500
+    resized = img.resize((500, 500), Image.LANCZOS)
+    png_filename = os.path.join(output_dir, f"logo.png")
+    resized.save(png_filename, format='png')
+    print(f"生成: {png_filename}")
+
 # 示例用法
 if __name__ == "__main__":
-    png_file = "touxiang.png"          # 你的 PNG 文件
-    output_folder = "ico_outputs"     # 输出目录
+    
     generate_individual_ico(png_file, output_folder)
